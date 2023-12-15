6224025b89b14f26314d3eb2a671bd4cfcbfc7fd
chs12090 <104214623+chs12090@users.noreply.github.com>
2023-12-15 11:23:15 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,20 +1,3 @@
-// $(document).ready(function() {
-//     $("#btn1").click(function(){
-//         if ($("body").css('background-color') === 'rgb(0, 0, 0)') {
-//             $("*").css('background-color', '');
-//             $("h1").html('<p style="color:black">WHITE THEME</p>'); 
-//             $("b,p,h2").css('color','black'); 
-//             $("#btn1").html('<div style="color:black">Dark<i class="fa fa-moon-o"></i></div>');
-//         } 
-//         else {
-//             $("body").css('background-color','black');
-//             $('#btn1').css('background-color','black');
-//             $("h1").html('<p style="color:white"> BLACK THEME</p>'); 
-//             $('#btn1').css('color','white');
-//             $('p,b,h2').css('color','lightgreen');
-//             $("#btn1").html('<div style="color:white">Light <i class="fa fa-sun-o"></i></div>');         }
-//     });
-// });
 $(document).ready(function() {
     // Initial mode (light mode)
     var darkMode = false;
